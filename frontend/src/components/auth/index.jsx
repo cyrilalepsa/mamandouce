0899fa361a60fd9fcb3d9019bfc82fbe8349ea2e
cyrilalepsa <cyrilalepsa@gmail.com>
2026-05-26 23:00:30 +0200

    Create index.jsx

---
+++ frontend/src/components/auth/index.jsx
@@ -0,0 +1,7 @@
+export { LoginForm } from './LoginForm';
+export { RegisterForm } from './RegisterForm';
+export { TwoFactorInput } from './TwoFactorInput';
+export { ForgotPasswordForm } from './ForgotPasswordForm';
+export { BiometricPrompt } from './BiometricPrompt';
+export { PinSetupPrompt, PinSetupInput } from './PinSetup';
+export { BiometricLoginButton, PinLoginSection } from './QuickLogin';
