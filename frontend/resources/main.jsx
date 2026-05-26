2afe1e04c7d8320d926fa9b2f0eb0015fa419431
cyrilalepsa <cyrilalepsa@gmail.com>
2026-05-26 22:58:56 +0200

    Create main.jsx

---
+++ frontend/resources/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Assurez-vous d'avoir un fichier App.jsx dans src
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
