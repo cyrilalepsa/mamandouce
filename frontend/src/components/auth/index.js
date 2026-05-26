06789e1d43f167e360fb1100af1b36ff75385be6
cyrilalepsa <cyrilalepsa@gmail.com>
2026-05-26 23:00:10 +0200

    Delete index.js

--- frontend/src/components/auth/index.js
+++
@@ -1,7 +0,0 @@
-export { LoginForm } from './LoginForm';
-export { RegisterForm } from './RegisterForm';
-export { TwoFactorInput } from './TwoFactorInput';
-export { ForgotPasswordForm } from './ForgotPasswordForm';
-export { BiometricPrompt } from './BiometricPrompt';
-export { PinSetupPrompt, PinSetupInput } from './PinSetup';
-export { BiometricLoginButton, PinLoginSection } from './QuickLogin';
