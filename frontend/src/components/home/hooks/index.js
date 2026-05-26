3f6e5fc273abea2c5bdd6999ec1715f16e8ccb8b
cyrilalepsa <cyrilalepsa@gmail.com>
2026-05-26 23:03:15 +0200

    Delete index.js

--- frontend/src/components/home/hooks/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Index des hooks personnalisés pour la page d'accueil
- * Permet des imports simplifiés
- */
-
-export { useSwipeNavigation } from './useSwipeNavigation';
-export { useLongPress } from './useLongPress';
-export { useDragAndDrop } from './useDragAndDrop';
